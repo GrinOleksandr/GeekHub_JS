f983d7fdac117884da54f02ba9a9cf212f105a45
Oleksandr Grin <44158680+GrinOleksandr@users.noreply.github.com>
2019-03-01 16:03:19 +0200

    Update About.js

--- HomeWork18(React-Router)/Client/src/Components/About/About.js
+++ HomeWork18(React-Router)/Client/src/Components/About/About.js
@@ -6,7 +6,7 @@ class About extends Component {
         return (
 
             <div id="content">
-                <p> Hi! My name is Oleksandr.I'm 33 years old and i'm a Christian. I have 1 wife, her name is Valentina, and 2 kids - Ivan and Tonya. Now i'm living
+                <p> Hi! My name is Oleksandr.I'm 33 years old and I'm a Christian. I have 1 wife, her name is Valentina, and 2 kids - Ivan and Tonya. Now I'm living
                     in Ukraine, near Smila city with my family. I'm now a junior JavaScript developer studying at GeekHub courses at Cherkasy,
                     and looking for a new ways to develop my skills of JavaScript and to master new technologies.</p>
             </div>
